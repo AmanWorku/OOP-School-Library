e2d50fb7f11259a2b60334d629480a17ada16aaa
AmanWorku <amexworku2014@gmail.com>
2023-03-13 20:40:07 +0300

    Fix linters

--- Person.rb
+++ Person.rb
@@ -1,7 +1,7 @@
-# frozen_string_literal: true
+# Person section
 
 class Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,12 +11,12 @@ class Person
   attr_accessor :age, :name
   attr_reader :id
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 end
 
--- Student.rb
+++ Student.rb
@@ -1,7 +1,7 @@
-# frozen_string_literal: true
+# Student section
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
