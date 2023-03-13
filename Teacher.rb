e641a5dd840f75e281135792b862ad539dfa335b
AmanWorku <amexworku2014@gmail.com>
2023-03-13 18:13:41 +0300

    Inherit and refactor code teacher

---
+++ Teacher.rb
@@ -0,0 +1,9 @@
+class Teacher < Person
+    def initialize(age, name="Unknown", parent_permission=true, specialization)
+        super(age, name, parent_permission)
+        @specialization = specialization
+      end
+    def can_use_services?
+        true
+    end
+end
