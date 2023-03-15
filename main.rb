142457024fca915bc782046eddef99cd5a441dcd
AmanWorku <amexworku2014@gmail.com>
2023-03-15 13:13:49 +0300

    Add interface for main.rb

---
+++ main.rb
@@ -0,0 +1,38 @@
+require './app'
+
+def main
+    loop do
+
+    app = App.new();
+    puts "Welcome to School Library"
+    puts "\n"
+    puts "1 - List all books"
+    puts "2 - List all people"
+    puts "3 - Create a person"
+    puts "4 - Create a book"
+    puts "5 - Create a rental"
+    puts "6 - List all rentals for a given person id"
+    puts "7 - Exit"
+
+        input = gets.champ
+    
+        case test
+        when 1
+            
+        when 2
+
+        when 3
+
+        when 4
+
+        when 5
+
+        when 6
+
+        when 7
+            
+        else
+            
+        end
+    end
+end
