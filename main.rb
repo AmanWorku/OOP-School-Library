b632392fcfe7fe752e9ae1e53162612c5d40b36f
AmanWorku <amexworku2014@gmail.com>
2023-03-20 16:57:36 +0300

    Create actions file for listing performable actions

--- main.rb
+++ main.rb
@@ -5,37 +5,7 @@ class Main
     @app = App.new
   end
   puts 'Welcome to School Library'
-  def run
-    loop do
-      puts "\n"
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-
-      input = gets.chomp
-
-      case input.to_i
-      when 1
-        @app.list_books
-      when 2
-        @app.list_people
-      when 3
-        @app.create_person
-      when 4
-        @app.create_book
-      when 5
-        @app.create_rental
-      when 6
-        @app.list_rentals
-      when 7
-        return
-      end
-    end
-  end
+  
 end
 
 main = Main.new
