4ae07eb4c1ac9d1dc9827454246fdc1b522ebd88
Salwa99 <s.ritage99@gmail.com>
2023-03-23 15:41:02 +0000

    Add tests to all methods in the class Person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,35 +2,84 @@ require_relative '../person'
 require_relative '../book'
 require_relative '../rental'
 
-describe Person do
-  let(:person) { Person.new(20, 'Lolo', parent_permission: true) }
+RSpec.describe Person do
+  let(:person) { Person.new(18) }
 
-  it 'Check if it is an instance of the class Person' do
-    expect(person).to be_instance_of(Person)
+  describe '#initialize' do
+    it 'Return a new Person object' do
+      expect(person).to be_an_instance_of Person
+    end
+
+    it 'Assigns the age' do
+      expect(person.age).to eq 18
+    end
+
+    it 'Assign a default name if no name is given' do
+      expect(person.name).to eq 'Unknown'
+    end
+
+    it 'Assigns parent permission to true by default' do
+      expect(person.instance_variable_get(:@parent_permission)).to eq true
+    end
+
+    it 'Assigns an id between 1 and 1000' do
+      expect(person.id).to be_between(1, 1000)
+    end
+
+    it 'Initializes an empty rentals array' do
+      expect(person.rentals).to be_empty
+    end
   end
 
-  it 'Check the name of the person' do
-    expect(person.name).to eq('Lolo')
+  describe '#of_age?' do
+    it 'Return true if age is 18 or greater' do
+      expect(person.of_age?).to eq true
+    end
+
+    it 'Return false if age is less than 18' do
+      person.age = 17
+      expect(person.of_age?).to eq false
+    end
   end
 
-  it 'Check the age of the person' do
-    expect(person.age).to eq(20)
+  describe '#can_use_services?' do
+    it 'Return true if age is 18 or greater' do
+      expect(person.can_use_services?).to eq true
+    end
+
+    it 'Return true if parent permission is granted' do
+      person.instance_variable_set(:@parent_permission, true)
+      person.age = 15
+      expect(person.can_use_services?).to eq true
+    end
   end
 
-  it 'Check if the person can use services' do
-    expect(person.can_use_services?).to be_truthy
+  describe '#to_h' do
+    let(:book) { Book.new('Babel', 'B.Salwa') }
+    let(:date) { '12-02-2020' }
+  
+    it 'Return a hash' do
+      expected_hash = {
+        age: 18,
+        id: person.id,
+        name: 'Unknown',
+        parent_permission: true,
+        rentals: []
+      }
+      expect(person.to_h).to eq(expected_hash)
+    end
+  end
+  
+  
+    describe '#correct_name' do
+      it 'Return the correct name of the person' do
+        person.name = 'Lolo'
+        expect(person.correct_name).to eq('Lolo')
+      end
+  
+      it 'Returns the default name if no name is given' do
+        expect(person.correct_name).to eq('Unknown')
+      end
+    end
   end
 
-  it 'Check the rentals of the person' do
-    expect(person.rentals).to be_empty
-  end
-
-  it 'The add_rental method returns a rental and adds it to the book' do
-    book = Book.new('Title', 'Author')
-    person = Person.new(20, 'Lolo')
-    puts "rentals before add_rental: #{person.rentals}"
-    rental = person.add_rental(book, '12/05/2023')
-    puts "rentals after add_rental: #{person.rentals}"
-    expect(person.rentals).to eql [rental]
-  end
-end
