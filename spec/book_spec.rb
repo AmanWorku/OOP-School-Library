c93cb3ca90010c85f319c4b49e684d2a7a4604df
Salwa99 <s.ritage99@gmail.com>
2023-03-23 15:09:26 +0000

    Add more tests for class book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -26,4 +26,25 @@ RSpec.describe Book do
       expect(book.rentals).to be_empty
     end
   end
+
+  describe '#add_rental' do
+    let(:person) { double('person') }
+    let(:date) { double('date') }
+
+    it 'Create a new Rental object with the given person and date' do
+      rental = book.add_rental(person, date)
+      expect(rental).to be_an_instance_of Rental
+      expect(rental.book).to eq book
+      expect(rental.person).to eq person
+      expect(rental.date).to eq date
+    end
+
+    it 'Add the new rental to the rentals array' do
+      book.add_rental(person, date)
+      expect(book.rentals.length).to eq 1
+      expect(book.rentals.first.book).to eq book
+      expect(book.rentals.first.person).to eq person
+      expect(book.rentals.first.date).to eq date
+    end
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../person'
+require_relative '../book'
+require_relative '../rental'
 
 describe Person do
   let(:person) { Person.new(20, 'Lolo', parent_permission: true) }
 
-  it 'Chech if it is an instance of the class Person' do
+  it 'Check if it is an instance of the class Person' do
     expect(person).to be_instance_of(Person)
   end
 
@@ -21,5 +23,14 @@ describe Person do
 
   it 'Check the rentals of the person' do
     expect(person.rentals).to be_empty
+  end
+
+  it 'The add_rental method returns a rental and adds it to the book' do
+    book = Book.new('Title', 'Author')
+    person = Person.new(20, 'Lolo')
+    puts "rentals before add_rental: #{person.rentals}"
+    rental = person.add_rental(book, '12/05/2023')
+    puts "rentals after add_rental: #{person.rentals}"
+    expect(person.rentals).to eql [rental]
   end
 end
